cab16f9ec0e20cf932611980c7c75a99550ae052
airatishimbaev <ufarobot@gmail.com>
2026-06-01 22:05:17 +0300

    deploy: site updates

--- src/data/site.js
+++ src/data/site.js
@@ -2,12 +2,11 @@ export const site = {
   name: 'learncs.ru',
   url: 'https://ufarobot.github.io/learncs/',
   description: 'Олимпиадная информатика для школьников 5-11 классов.',
-  stylesheetVersion: '20260530-curriculum',
+  stylesheetVersion: '20260601-curriculum-toggle',
   scriptVersion: '20260530-order-form'
 };
 
 export const nav = [
-  { label: 'Курс', href: './' },
   { label: 'Учебник', href: 'https://book.learncs.ru' }
 ];
 
